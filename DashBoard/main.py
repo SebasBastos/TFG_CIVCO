7119aae4d3127ce29947c7037808cb4e0d1b21db
Sebastian Bastos <sebastos@estudiantec.cr>
2025-08-14 14:34:21 -0600

    haciendo pruebas para el dashboard

--- DashBoard/main.py
+++ DashBoard/main.py
@@ -13,6 +13,17 @@ print("Loading main.py...")
 # https://www.youtube.com/@ProgrammingIsFun
 
 # Set the page configuration for the Streamlit app
-st.set_page_config(page_title="TFG CIVCO Dashboard", page_icon=":bar_chart:", layout="wide")
-st.title("TFG CIVCO Dashboard")
+st.set_page_config(page_title="eBridge Dashboard", page_icon=":bar_chart:", layout="wide")
+st.title(":bridge_at_night: TFG CIVCO Dashboard")
+st.markdown("<style>div.block-container {padding-top:1rem;} </style>", unsafe_allow_html=True)
 
+fl = st.file_uploader(":file_folder: Upload your file", type=(["csv", "txt", "xlsx", "xls"]))
+
+if fl is not None:
+    filename = fl.name
+    st.write(f"File uploaded: {filename}")
+    df = pd.read_csv(filename, encoding = "ISO-8859-1")
+else:
+    os.chdir(os.path.dirname(__file__))
+    df = pd.read_csv("../data/processed_data.csv", encoding = "ISO-8859-1")
+    
